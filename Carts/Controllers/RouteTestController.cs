4eb193143c55262c023b0f19060ff276041eb8fa
DESKTOP-1TVCHEP\Calaeno <mxm908@gmail.com>
2021-02-26 14:03:43 +0800

    first add

---
+++ Carts/Controllers/RouteTestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carts.Controllers
+{
+    public class RouteTestController : Controller
+    {
+        // GET: RouteTest
+        public ActionResult Index()
+        {
+            var result = Models.RouteTest.TempProducts.getAllproducts();
+            return View(result);
+        }
+
+        public ActionResult Index2(string id)
+        {
+            return Content(
+                string.Format("id的值為:{0}", id));
+        }
+
+        public ActionResult Index3(string name ,string id)
+        {
+            return Content(
+                string.Format("使用者為{0},ID為{1}",name,id)
+                );
+        }
+    }
+}
---
+++ Carts/Models/Cart/Operation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Carts.Models.Cart
+{
+    //購物車操作類別
+    public static class Operation
+    {
+        [WebMethod(EnableSession = true)]   //啟用session
+        public static Models.Cart.Cart GetCurrentCart() //取得目前Session中的Cart物件
+        {
+            if(System.Web.HttpContext.Current != null)  //確認System.web.HttpContext.Current是否為空
+            {
+                //如果Session["Cart"]不存在,則新增一個空的Cart物件
+                if (System.Web.HttpContext.Current.Session["Cart"] == null)
+                {
+                    var order = new Cart();
+                    System.Web.HttpContext.Current.Session["Cart"] = order;
+                }
+
+                //回傳Session["Cart"]
+                return (Cart)System.Web.HttpContext.Current.Session["Cart"];
+            }
+            else
+            {
+                throw new InvalidOperationException("System.web.HttpContext.Current為空,請檢查");
+            }
+        }
+    }
+}
